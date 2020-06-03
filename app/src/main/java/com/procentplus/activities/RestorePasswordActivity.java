c0326f7553f69aadd641559e45540e598a4fe95f
kair32 <aleksanov.96@gmail.com>
2020-06-03 19:35:39 +0300

    fixed restore password with send message

--- app/src/main/java/com/procentplus/activities/RestorePasswordActivity.java
+++ app/src/main/java/com/procentplus/activities/RestorePasswordActivity.java
@@ -1,44 +1,31 @@
 package com.procentplus.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Paint;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.UnderlineSpan;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.procentplus.ProgressDialog.DialogConfig;
 import com.procentplus.R;
-import com.procentplus.retrofit.RetrofitClient;
-import com.procentplus.retrofit.interfaces.INewPassword;
-import com.procentplus.retrofit.models.NewPassword;
-import com.procentplus.retrofit.models.NewPasswordReq;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
 
 public class RestorePasswordActivity extends AppCompatActivity implements View.OnClickListener{
 
     @BindView(R.id.restore_back_btn)
     ImageView restore_back_btn;
-    @BindView(R.id.send_email_restore)
-    Button btn_send_email_restore;
     @BindView(R.id.register_btn_restore)
     TextView register_btn_restore;
-    @BindView(R.id.et_restore_email)
-    EditText et_restore_email;
-
-    private Retrofit retrofit;
-    private INewPassword iNewPassword;
-    private DialogConfig progressDialog;
+    @BindView(R.id.textView)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,18 +33,17 @@ public class RestorePasswordActivity extends AppCompatActivity implements View.O
         setContentView(R.layout.activity_restore_password);
         ButterKnife.bind(this);
 
-        // init api
-        retrofit = RetrofitClient.getInstance();
-
-        // init dialog
-        progressDialog = new DialogConfig(this, "Идет загрузка");
+        SpannableString content = new SpannableString("Если не можете войти в приложение, напишите на info@procentplus.com");
+        content.setSpan(new UnderlineSpan(), 47, content.length(), 0);
+        content.setSpan(new ForegroundColorSpan(Color.BLUE), 47, content.length(), 0);
+        textView.setText(content);
 
         // underlining text views
         register_btn_restore.setPaintFlags(register_btn_restore.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
         // init click listeners
         restore_back_btn.setOnClickListener(this);
-        btn_send_email_restore.setOnClickListener(this);
+        textView.setOnClickListener(this);
         register_btn_restore.setOnClickListener(this);
     }
 
@@ -67,63 +53,16 @@ public class RestorePasswordActivity extends AppCompatActivity implements View.O
             case R.id.restore_back_btn:
                 onBackPressed();
                 break;
-            case R.id.send_email_restore:
-                String restore_email = et_restore_email.getText().toString().trim();
-                if (!restore_email.isEmpty()) {
-                    restorePassword(restore_email);
-
-                    // show dialog
-                    progressDialog.showDialog();
-
-                } else {
-                    MainActivity.prefConfig.displayToast("Введите свой email");
-                }
-                break;
             case R.id.register_btn_restore:
                 Intent intent = new Intent(RestorePasswordActivity.this, RegisterActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.textView:
+                Intent mailIntent = new Intent(Intent.ACTION_VIEW);
+                Uri data = Uri.parse("mailto:?subject=" + "Восстановление доступа"+ "&body=" + "Я забыл свой пароль. Пожалуйста напомните. \nМой номер:" + "&to=" + "info@procentplus.com");
+                mailIntent.setData(data);
+                startActivity(Intent.createChooser(mailIntent, "Send mail..."));
+                break;
         }
-    }
-
-    private void restorePassword(String restore_email) {
-        iNewPassword = retrofit.create(INewPassword.class);
-
-        Call<NewPassword> newPasswordCall = iNewPassword.newPasswordData(
-                new NewPassword(new NewPasswordReq(restore_email))
-        );
-
-        newPasswordCall.enqueue(new Callback<NewPassword>() {
-            @Override
-            public void onResponse(Call<NewPassword> call, Response<NewPassword> response) {
-                int statusCode = response.code();
-                Log.d("LOGGER Reg", "statusCode: " + statusCode);
-                if (statusCode == 200) {
-                    // hide dialog
-                    progressDialog.dismissDialog();
-                    MainActivity
-                            .prefConfig
-                            .displayToast("Для сброса пароля перейдите по ссылке, которая была отправлена на вашу почту");
-                    Intent intent = new Intent(RestorePasswordActivity.this, AuthActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    // hide dialog
-                    progressDialog.dismissDialog();
-                    MainActivity
-                            .prefConfig
-                            .displayToast("Произошла ошибка при отправлении запроса. Попробуйте снова");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<NewPassword> call, Throwable t) {
-                // hide dialog
-                progressDialog.dismissDialog();
-                MainActivity
-                        .prefConfig
-                        .displayToast("Произошла ошибка при отправлении запроса. Попробуйте снова");
-            }
-        });
     }
 }
