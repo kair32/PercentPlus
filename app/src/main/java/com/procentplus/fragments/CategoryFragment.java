86b400495a41c79838eb330ad5b5b71773d9e5c7
kair32 <aleksanov.96@gmail.com>
2020-06-19 12:37:30 +0300

    added logout in category

--- app/src/main/java/com/procentplus/fragments/CategoryFragment.java
+++ app/src/main/java/com/procentplus/fragments/CategoryFragment.java
@@ -1,5 +1,6 @@
 package com.procentplus.fragments;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -12,14 +13,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.procentplus.R;
+import com.procentplus.activities.AuthActivity;
 import com.procentplus.activities.MainActivity;
 import com.procentplus.adapter.CategoryAdapter;
 import com.procentplus.retrofit.RetrofitClient;
 import com.procentplus.retrofit.interfaces.IAuthorization;
 import com.procentplus.retrofit.interfaces.ICategories;
+import com.procentplus.retrofit.interfaces.ILogout;
 import com.procentplus.retrofit.models.AuthResponse;
 import com.procentplus.retrofit.models.CategoriesResponse;
 import com.procentplus.retrofit.models.MobileUser;
@@ -58,6 +62,9 @@ public class CategoryFragment extends Fragment {
         categoryRecyclerView = view.findViewById(R.id.categoryRecyclerView);
 
         getCategories();
+
+        ImageView logout_btn = view.findViewById(R.id.logout);
+        logout_btn.setOnClickListener(view1 -> logout());
 
         TextView tv_category_city = view.findViewById(R.id.tv_category_city);
         tv_category_city.setText("Курск");
@@ -132,5 +139,30 @@ public class CategoryFragment extends Fragment {
         mAdapter = new CategoryAdapter(categoriesList);
         categoryRecyclerView.addItemDecoration(itemDecorator);
         categoryRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void logout() {
+        ILogout iLogout = retrofit.create(ILogout.class);
+
+        Call<CategoriesResponse> call = iLogout.logOut(MainActivity.prefConfig.readToken());
+
+        call.enqueue(new Callback<CategoriesResponse>() {
+            @Override
+            public void onResponse(Call<CategoriesResponse> call, Response<CategoriesResponse> response) {
+                int statusCode = response.code();
+                Log.d("LOGGER Logout", "statusCode: " + statusCode);
+                if (statusCode == 200 || statusCode == 204) {
+                    MainActivity.prefConfig.writeLoginStatus(false);
+                    Intent intent = new Intent(getContext(), AuthActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CategoriesResponse> call, Throwable t) {
+                MainActivity.prefConfig.displayToast("Произошла ошибка при попытке выхода из профиля");
+            }
+        });
     }
 }
